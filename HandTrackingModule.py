24a73810624a68c3947f9c9e61bb171e81f58abd
prabhjyot009 <prabh156dps@gmail.com>
2023-11-24 16:42:41 +0530

    #commit

--- HandTrackingModule.py
+++ HandTrackingModule.py
@@ -1,66 +1,84 @@
 import cv2
-import mediapipe as mp
 import time
-import math
+import mediapipe as mp
+
 
 class handDetector():
-    def __init__(self,mode=False,maxHands=2,detectionCon=0.5,trackCon=0.5):
+    def __init__(self, mode=False, maxHands=2,modelC=1, detectionCon=0.5, trackCon=0.5):
         self.mode = mode
         self.maxHands = maxHands
+        self.modelC = modelC
         self.detectionCon = detectionCon
         self.trackCon = trackCon
-        # Hand tracking module
-        self.mpHands = mp.solutions.hands
-        self.hands = self.mpHands.Hands(self.mode,self.maxHands,self.detectionCon,self.trackCon)
-        self.mpDraw = mp.solutions.drawing_utils
-    def findHands(self,img,draw=True):
-        imgRGB = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
-        results = self.hands.process(imgRGB)
-        print(results.multi_hand_landmarks)
 
-        if results.multi_hand_landmarks:
-            for handLms in results.multi_hand_landmarks:
-                self.mpDraw.draw_landmarks(img, handLms, self.mpHands.HAND_CONNECTIONS)
+        self.mpHands = mp.solutions.hands
+        self.hands = self.mpHands.Hands(self.mode, self.maxHands,self.modelC, self.detectionCon, self.trackCon)
+        self.mpDraw = mp.solutions.drawing_utils
+        self.tipIds = [4, 8, 12, 16, 20]
+
+    def findHands(self, img, draw=True):
+        imgRGB = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+        self.result = self.hands.process(imgRGB)
+        # print(result.multi_hand_landmarks)
+
+        if self.result.multi_hand_landmarks:
+            for handLand in self.result.multi_hand_landmarks:
+                if draw:
+                    self.mpDraw.draw_landmarks(img, handLand, self.mpHands.HAND_CONNECTIONS)
+
         return img
 
-    for id, lm in enumerate(handLms.landmark):
-        # print(id,lm)
-        h, w, c = img.shape
-        cx, cy = int(lm.x * w), int(lm.y * h)
-        print(id, cx, cy)
-        if id == 0:
-            cv2.circle(img, (cx, cy), 15, (255, 0, 255), cv2.FILLED)
+    def findPosition(self, img, handNo=0, draw=True):
+        self.PosList = []
+        if self.result.multi_hand_landmarks:
+            myHand = self.result.multi_hand_landmarks[handNo]
+            for id, lm in enumerate(myHand.landmark):
+                h, w, c = img.shape
+                cx, cy = int(lm.x * w), int(lm.y * h)
+                self.PosList.append([id, cx, cy])
 
-    def findPosition(self,img,handNo=0,draw=True):
-        lmList = []
-        if self.results.multi_hand_landmarks:
-            myHand = self.results.multi_hand_landmarks[handNo]
-            for id,lm in enumerate(myHand.landmark):
-                    #print(id,lm)
-                    h,w,c = img.shape
-                    cx,cy = int(lm.x*w),int(lm.y*h)
-                    #print(id,cx,cy)
-                    lmList.append([id,cx,cy])
-                    if draw:
-                        cv2.circle(img,(cx,cy),15,(255,0,255),cv2.FILLED)
-        return lmList
+                if draw:
+                    cv2.circle(img, (cx, cy), 15, (255, 0, 255), cv2.FILLED)
+
+        return self.PosList
+
+    def fingerUp(self):
+        fingers = []
+        if self.PosList[self.tipIds[0]][1] < self.PosList[self.tipIds[0] - 1][1]:
+            fingers.append(1)
+        else:
+            fingers.append(0)
+        for id in range(1, 5):
+
+            if self.PosList[self.tipIds[id]][2] < self.PosList[self.tipIds[id] - 2][2]:
+                fingers.append(1)
+            else:
+                fingers.append(0)
+        return fingers
+
 def main():
     pTime = 0
     cTime = 0
-
-    # VideoCapture(0) is the default camera
-    cap = cv2.VideoCapture(1)
+    cap = cv2.VideoCapture(0)
+    detector = handDetector()
 
     while True:
         success, img = cap.read()
+        img = detector.findHands(img)
+        PosList = detector.findPosition(img)
+
+        if len(PosList) != 0:
+            print(PosList[4])
 
         cTime = time.time()
         fps = 1 / (cTime - pTime)
         pTime = cTime
-        # Display FPS
-        cv2.putText(img, str(int(fps)), (10, 70), cv2.FONT_HERSHEY_PLAIN, 3, (255, 0, 255), 3)
-        # Display image
-        cv2.imshow("Image", img)
+
+        cv2.putText(img, str(int(fps)), (10, 70), cv2.FONT_HERSHEY_SCRIPT_COMPLEX, 3, (255, 0, 255), 3)
+
+        cv2.imshow("Webcam", img)
         cv2.waitKey(1)
+
+
 if __name__ == "__main__":
     main()
