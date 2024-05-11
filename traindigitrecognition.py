b9bac8dc630e9b5021dc7dfa43a955e1897ed604
prabhjyot009 <prabhjyot009s@gmail.com>
2024-05-11 18:33:51 +0530

    changes in classification reports and addition of ROC curve

--- traindigitrecognition.py
+++ traindigitrecognition.py
@@ -52,3 +52,23 @@ plt.show()
 #Classification Report
 from sklearn.metrics import classification_report
 print(classification_report(y_true, y_pred_classes))
+
+#ROC Curve
+from sklearn.metrics import roc_curve
+from sklearn.metrics import roc_auc_score
+fpr = dict()
+tpr = dict()
+roc_auc = dict()
+for i in range(10):
+    fpr[i], tpr[i], _ = roc_curve(test_labels[:, i], y_pred[:, i])
+    roc_auc[i] = roc_auc_score(test_labels[:, i], y_pred[:, i])
+plt.figure()
+plt.plot(fpr[2], tpr[2], color='darkorange', lw=2, label='ROC curve (area = %0.2f)' % roc_auc[2])
+plt.plot([0, 1], [0, 1], color='navy', lw=2, linestyle='--')
+plt.xlim([0.0, 1.0])
+plt.ylim([0.0, 1.05])
+plt.xlabel('False Positive Rate')
+plt.ylabel('True Positive Rate')
+plt.title('Receiver Operating Characteristic')
+plt.legend(loc="lower right")
+plt.show()
